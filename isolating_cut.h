3bfab96dfb73b2a1ac784ad6621bb369d8c6e879
Kyle Campbell <elyk.llebpmac@gmail.com>
2021-10-19 11:30:24 -0400

    isolating cut

--- isolating_cut.h
+++ isolating_cut.h
@@ -2,180 +2,312 @@
 #include <list>
 #include <limits>
 #include <map>
-#include <set>
+
+#include <math.h>
 
 #include <lemon/list_graph.h>
 #include <lemon/concepts/graph.h>
-#include <lemon/bits/map_extender.h>
+#include <lemon/concepts/graph_components.h>
 #include <lemon/bits/graph_extender.h>
+#include <lemon/concepts/bpgraph.h>
+
 #include <lemon/preflow.h>
-#include <lemon/tolerance.h>
-#include <lemon/color.h>
+#include <lemon/dfs.h>
 #include <lemon/core.h>
 
-// write python pseudocode and then c++
+// TODO move func defs to .cc file
 
 namespace lemon {
+    template <typename GR, typename CAP>
+    struct LiPanigrahiDefaultTraits {
+
+    typedef GR ListGraph;
+
+    typedef CAP CapacityMap;
+
+#ifdef DOXYGEN
+    typedef GR::NodeMap<Value> MinCutMap;
+#else
+    typedef typename ListGraph::template NodeMap<bool> MinCutMap;
+#endif
+
+    static MinCutMap* createCutMap(const ListGraph& graph) {
+      return new MinCutMap(graph);
+    }
+
+
+#ifdef DOXYGEN
+    typedef GR::EdgeMap<Value> MinCutEdgeMap;
+#else
+    typedef typename ListGraph::template EdgeMap<bool> MinCutEdgeMap;
+#endif
+
+    static MinCutEdgeMap* createCutEdgeMap(const ListGraph& graph) {
+      return new MinCutEdgeMap(graph);
+    }
+
+typedef typename std::vector<int> NodeIds;
+
+typedef struct SV {
+    int weight;
+    NodeIds sv;
+} SV;
+
+};
+
+
 
 #ifdef DOXYGEN
   template <typename GR, typename CAP, typename TOL>
 #else
   template <typename GR,
             typename CAP = typename GR::template EdgeMap<int>,
-            typename TOL = Tolerance<typename CAP::Value> >
+            typename TR = LiPanigrahiDefaultTraits<GR, CAP> >
 #endif
 
     class IsolatingCut {
         
     public:
+        typedef TR Traits;
         /// The graph type of the algorithm
         typedef GR ListGraph;
         /// The capacity map type of the algorithm
         typedef CAP CapacityMap;
-        // /// The tolerance type of the algorithm
-        // typedef TOL Tolerance;
 
+        typedef typename Traits::SV SV;
+
+        typedef typename Traits::MinCutMap MinCutMap;
+
+        typedef typename Traits::MinCutEdgeMap MinCutEdgeMap;
+
+        typedef typename Traits::NodeIds NodeIds;
+       
     private:
 
         typedef typename CapacityMap::Value Value;
 
         TEMPLATE_GRAPH_TYPEDEFS(ListGraph);
 
-        const ListGraph& _graph;
-        const CapacityMap& _capacity;
-        typedef typename ListGraph::template NodeMap<bool> MinCutMap;
-        const MinCutMap* _cutmap;
-
-        const std::vector<Node>& _subset;
-        std::map<Node, int32_t> _labels;
-        std::map<int, std::vector<Node>& > _colorings;
-        // we need some struct of saving the logV colorings
-        // typedef typename ListGraph::template EdgeMap<Color> ColorMap;
-        // ColorMap _color_mincuts;
-        
     public:
-    IsolatingCut(const ListGraph& graph, const CapacityMap& capacity, const vector<Node>& subset) :
-        _graph(graph), _capacity(capacity), _subset(subset) {
-            init();
-        }
+    IsolatingCut(const ListGraph& graph, CapacityMap& capacity, vector<Node>& subset, ListGraph& c1, ListGraph& c2):
+        _capacity(&capacity), _subset(&subset), _ogGraph(&copyGraph(c1, graph)), _graph(&copyGraph(c2, graph)),
+        _s((*_graph).addNode()), _t((*_graph).addNode()), _maxWeight(graphSumWeight(capacity))
+    {}
 
     ~IsolatingCut() {
-        if (_graph) {
-            delete _graph;
-        }
-        if (_capacity) {
-            delete _capacity;
-        }
-        if (_subset) {
-            delete _subset;
-        }
-        if (_labels) {
-            delete _labels;
-        }
-        if (_colorings) {
-            delete _colorings;
-        }
+        delete _allCutsUnion;
+        delete _cutmap;
+        delete isolatingCuts;
     }
+    
+    private: 
+        // graph pointer to original graph
+        ListGraph* _ogGraph;
+        // graph we will find s-t cuts and create edges on
+        ListGraph* _graph;
+        CapacityMap* _capacity;
+        Node _s, _t;
+        int _maxWeight = 0;
+
+        
+        MinCutEdgeMap* _allCutsUnion;
+        MinCutMap* _cutmap; 
+
+        std::vector<SV>* isolatingCuts; // eventually make this thread safe
+
+        std::vector<Node>* _subset;
  
     private:
-        void initLabels(std::vector<Node>& _subset) {
-            int len = _subset.size();
-            for (int i = 0; i < len; ++i) {
-                _labels[_subset[i]] = i;
-            }
+        void createStructures() {
+
+            // structures for storing results from log|V| maxflow comps
+            _allCutsUnion = TR::createCutEdgeMap(*_ogGraph);
+            _cutmap = TR::createCutMap(*_ogGraph);
+            isolatingCuts = new std::vector<SV>;
         }
-        void copyGraph(ListGraph& new_graph, ListGraph& orig_graph) {
-            GraphCopy<ListGraph, ListGraph> cg(orig_graph, new_graph);
-            cg.run();
-        }
-        std::list<Node> colorContraction(ListGraph& graph) {
-            std::list<Node> st;
-            std::vector<int> colors = {0, 1};
-            for (const auto& color : colors) {
-                std::vector<int, Node>& component = _colorings[color];
-                st.push_back(component[0]);
-                for (int i = 1; i < component.size(); ++i) {
-                    graph.contract(component[0], component[i]);
+
+        void removeCuts(ListGraph& trimGraph) {
+        
+            for (EdgeIt e(*_ogGraph); e != INVALID; ++e) {
+                if ((*_allCutsUnion)[e] == true) {
+                    trimGraph.erase(e);
                 }
             }
-            return st;
         }
-        void colorGraph(ListGraph& graph, int i) {
-            for (const auto& v : _subset) {
-                _colorings[findColor(_labels[v], i)].push_back(v);
+
+        ListGraph& copyGraph(ListGraph& new_graph, const ListGraph& orig_graph) {
+            ExtendedListGraphBase::NodeMap<ListGraphBase::Node> nr(orig_graph);
+            ExtendedListGraphBase::EdgeMap<ListGraphBase::Edge> ecr(new_graph);
+            GraphCopy<ListGraph, ListGraph> gc(orig_graph, new_graph);
+            gc.nodeRef(nr).edgeCrossRef(ecr);
+            gc.run();
+            return new_graph;
+        }
+
+        bool findColor(int label, int i) {
+            bool val = label & (1 << i);
+            return val;
+        }
+
+        int graphSumWeight(CAP& capacityMap) {
+            int weight = 0;
+            for (EdgeIt e(*_graph); e != INVALID; ++e) {
+                weight += capacityMap[e];
+            }
+            return weight;
+        }
+
+        int findMincuts(ListGraph& graph, Node s, Node t, MinCutMap& cutmap) {
+            Preflow<ListGraph, CapacityMap> pf(graph, *_capacity, s, t);
+            pf.run();
+            pf.minCutMap(cutmap);
+            return pf.flowValue();
+        }
+
+        // Uv is a mapping of the connected component of v
+        // contractGraph is a copy of the G/ union(mincuts)
+        Node contractUv(MinCutMap& Uv, ListGraph& contractGraph) {
+            Node firstNode;
+            bool fnSet = false;
+
+            // we can contract contractGraph off of og _graph bc it is a copy
+            for (NodeIt n(*_ogGraph); n != INVALID; ++n) {
+                if (! Uv[n]) {
+                    if (! fnSet) {
+                        fnSet = true;
+                        firstNode = n;
+                    }
+                    else {
+                        contractGraph.contract(firstNode, n);
+                    }
+                }
+            } 
+            return firstNode;
+        }
+
+        void findUv(const Node& v, MinCutMap& compMap, const ListGraph& isolatedCuts) {
+            Dfs<ListGraph> dfs(isolatedCuts);
+            dfs.reachedMap(compMap);
+            dfs.run(v);
+        }
+
+        void getCutNodeIds(std::vector<int>& nodeIds, MinCutMap& mincut, ListGraph& graph) {
+            for (NodeIt n(*_ogGraph); n != INVALID; ++n) {
+                if (mincut[n] == true) {
+                    nodeIds.push_back(graph.id(n));
+                }
             }
         }
 
-        int findColor(int label, int i) {
-            return (label >> i) & 1;
+        // once the isolated components are found, this finds isolating cuts
+        void parallelContractMinCut(ListGraph& isolatedComponents) {
+            // in parallel loop -- TODO
+            MinCutMap* connectedComponent = TR::createCutMap(*_ogGraph);
+            for (const auto& v : *_subset) {
+                
+                findUv(v, *connectedComponent, isolatedComponents);
+
+                ListGraph cgraph;
+                copyGraph(cgraph, *_ogGraph);
+
+                Node s = contractUv(*connectedComponent, cgraph);
+                int dSV = findMincuts(cgraph, s, v, *connectedComponent);
+                std::vector<int> nIds;
+                getCutNodeIds(nIds, *connectedComponent, *_ogGraph);
+                (*isolatingCuts).push_back(SV{dSV, nIds});
+                
+            }
+            delete connectedComponent;
         }
 
-        void findMincuts(ListGraph& graph, Node& s, Node& t) {
-            Preflow<ListGraph, CapacityMap> pf(graph, _capacity, *s, *t);
-            pf.run();
-            pf.minCutMap(_cutmap);
+        void undoSTComponents() {
+            for (OutArcIt a(*_graph, _s); a!=INVALID; ++a) {
+                (*_graph).erase(a);
+            };
+            for (InArcIt a(*_graph, _t); a!=INVALID; ++a) {
+                (*_graph).erase(a);
+            }
         }
 
-        void findIthMinCut(ListGraph& g, int i) {
-            colorGraph(g, i);
-            std::list<Node> st = colorContraction(g);
-            findMincuts(g, st[0], st[1]);
+        void addToCutUnion() {
+            for (typename ListGraph::ArcIt e(*_ogGraph); e != INVALID; ++e) {
+                if ((*_cutmap)[(*_graph).source(e)] != (*_cutmap)[(*_graph).target(e)]) {
+                    (*_allCutsUnion)[e] = true;
+                } 
+            }
+        }
+
+        void makeSTComponents(int i) {
+            int len = (*_subset).size();
+            for (int j = 0; j < len; ++j) {
+                Node tmp = (*_subset)[j];
+                if (findColor(j, i)) {
+                    (*_capacity)[(*_graph).addEdge(_s, tmp)] = _maxWeight;
+                }
+                else {
+                    (*_capacity)[(*_graph).addEdge(_t, tmp)] = _maxWeight;
+                }
+            }
+        }
+        
+        // removes edges for each of the log|V| calls
+        void findIthMinCut(int i) {
+            makeSTComponents(i);
+            int _ = findMincuts(*_graph, _s, _t, *_cutmap);
+            addToCutUnion();
+            undoSTComponents();
         }
 
     public:
+
         void init() {
-            initLabels(_subset);
+            createStructures();
         }
 
+        // must call IsolatingCut().init() before .run()
         void run() {
-            int len = _subset.size();
+            int len = ceil(log2((*_subset).size()));
             for (int i = 0; i < len; ++i) {
-                ListGraph newGraph;
-                copyGraph(newGraph, _graph);
-                findIthMinCut(newGraph, i);
+                findIthMinCut(i);
             }
+            ListGraph trimGraph;
+            copyGraph(trimGraph, *_graph);
+            removeCuts(trimGraph);
+            parallelContractMinCut(trimGraph);
+        }
+
+        std::vector<SV> getIsolatingCuts() {
+            return *isolatingCuts;
+        }
+
+        // test methods
+        int testIthMinCut(int i) {
+            makeSTComponents(i);
+            int cut = findMincuts(*_graph, _s, _t, *_cutmap);
+            return cut;
+        }
+
+        int getMaxWeight() {
+            return _maxWeight;
+        }
+
+        void testPhase1(Node& v, MinCutMap& compMap, Node& t) {
+            int len = ceil(log2((*_subset).size()));
+            for (int i = 0; i < len; ++i) {
+                findIthMinCut(i);
+            }
+            ListGraph trimGraph;
+            copyGraph(trimGraph, *_ogGraph);
+            removeCuts(trimGraph);
+            ExtendedListGraphBase::NodeMap<ListGraphBase::Arc> pm(trimGraph);
+
+            Dfs<ListGraph> dfs(trimGraph);
+            dfs.reachedMap(compMap);
+            dfs.run(v);
+            PredMapPath<ListGraph, ExtendedListGraphBase::NodeMap<ExtendedListGraphBase::Arc>> a = dfs.path(t);
         }
 
     }; //class IsolatingCut
 } //namespace lemon
 
-/*
-Public 
 
-what we need mincut for i colorings - use s-t maxflow w/ color contraction
-when finding these mincuts, denote which v in v is in the C_i and which side it is on
-find connected component with v
-union of C_i
-graph complement
-maxflow call for disjoint union of G_v
-recover S_v and w(S_v)
-
-
-class IsolatingCut
-
-    def __init__(subset: R):
-        self.ordering = R
-        self.labels = {}
-        self.colorings = {}
-        for i in range(len(R)):
-            label[R[i]] = binary(i)
-        
-        for i in range(log(len(R))):
-            for v in R:
-                color(v, labels[v], i)
-            mincut(C_i between red and blue in G) - use maxflow
-    
-    def _color(v, label: binary, i):
-        self.colorings[r] = blue if binary[i] == 1 else red
-
-    once C_i are found, U_v is subset in G \ U C_i that contains v
-    for each v
-    contract V \ U_v ito a single vertex t
-    compute min v-t cut on contracted graph (equivalent to S_v)
-    do these in parallel on all contracted graphs w/ max flow on disjoint union of contracted
-    (there are |v| of them)
-
-    recover the S_v and minimum weight across the S_v
-    return that then 
-
-*/
--- tests/test_isolating_cut.cc
+++ tests/test_isolating_cut.cc
@@ -1,21 +1,81 @@
 #include "test_graphs.h"
 #include "../isolating_cut.h"
+#include <lemon/list_graph.h>
+#include <lemon/concepts/graph.h>
+#include <lemon/concepts/bpgraph.h>
+#include <lemon/bits/graph_extender.h>
+#include <vector>
+#include <assert.h>
+#include <iostream>
 
+using namespace std;
+using namespace lemon;
+
+TEMPLATE_GRAPH_TYPEDEFS(ListGraph);
+typedef ListGraph::EdgeMap<int> CapacityMap;
+typedef IsolatingCut<ListGraph, CapacityMap> IsoCut;
 
 void testInitLabels() {
-
+  ListGraph g;
+  ListGraph::EdgeMap<int> map(g);
+  TestGraphs test(&g, &map);
+  test.createBicycleWheel(8);
+  vector<Node> subset = {g.nodeFromId(2), g.nodeFromId(4), g.nodeFromId(7)};
+  ListGraph c1, c2;
+  IsoCut ic(g, map, subset, c1, c2);
+  ic.init();
+  cout << ic.testIthMinCut(3) << endl;
+  // vector<SV> isolatingCuts = ic.getIsolatingCuts();
+  // cout << isolatingCuts.size() << endl;
+  // for (auto& cut : isolatingCuts) {
+  //   std::cout << cut.weight << endl;
+  //   for (int nodeId : cut.sv) {
+  //     std::cout << nodeId << endl;
+  //   }
+  // }
 };
 
-void testColorContradction() {
-
+void testMaxWeight() {
+  ListGraph g;
+  ListGraph::EdgeMap<int> map(g);
+  TestGraphs test(&g, &map);
+  test.createBicycleWheel(8);
+  vector<Node> subset = {g.nodeFromId(2), g.nodeFromId(4), g.nodeFromId(7)};
+  ListGraph c1, c2;
+  IsoCut ic(g, map, subset, c1, c2);
+  ic.init();
+  cout << ic.getMaxWeight() << endl;
 };
 
-void testColorGraph() {
-
+void testRun() {
+  ListGraph g;
+  ListGraph::EdgeMap<int> map(g);
+  TestGraphs test(&g, &map);
+  test.createBicycleWheel(16);
+  vector<Node> subset = {g.nodeFromId(7), g.nodeFromId(4), g.nodeFromId(12), g.nodeFromId(14)};
+  ListGraph c1, c2;
+  IsoCut ic(g, map, subset, c1, c2);
+  ic.init();
+  ic.run();
+  vector<LiPanigrahiDefaultTraits<ListGraph, ListGraph::EdgeMap<int>>::SV> isolatingCuts = ic.getIsolatingCuts();
 };
 
-int main()
-{
+void testDisconnection() {
+  ListGraph g;
+  ListGraph::EdgeMap<int> map(g);
+  TestGraphs test(&g, &map);
+  test.createBicycleWheel(8);
+  vector<Node> subset = { g.nodeFromId(2), g.nodeFromId(4), g.nodeFromId(1)};
+  ListGraph c1, c2;
+  IsoCut ic(g, map, subset, c1, c2);
+  ic.init();
+  ListGraph::NodeMap<bool> cutMap(g);
+  Node v = g.nodeFromId(2);
+  Node t = g.nodeFromId(4);
+  ic.testPhase1(v, cutMap, t);
+}
+
+void testSetup() {
   ListGraph g;
   ListGraph::EdgeMap<int> map(g);
   TestGraphs test(&g, &map);
@@ -32,8 +92,38 @@ int main()
     for (ListGraph::IncEdgeIt e(g, n); e != INVALID; ++e) {
         cnt++;
     }
-    // std::cout << "deg(" << g.id(n) << ") = " << cnt << std::endl;
-  };
+    std::cout << "deg(" << g.id(n) << ") = " << cnt << std::endl;
+  }
+};
 
-  return 0;
+void testConstructor() {
+  ListGraph g;
+  ListGraph::EdgeMap<int> map(g);
+  TestGraphs test(&g, &map);
+  test.createBicycleWheel(8);
+  vector<Node> subset = {g.nodeFromId(2), g.nodeFromId(4), g.nodeFromId(7)};
+  ListGraph c1, c2;
+  IsoCut ic(g, map, subset, c1, c2);
+  ic.init();
+  // assert (ic.getMaxWeight > 0);
 }
+
+int main() {
+  // testInitLabels();
+  testRun();
+
+  // ListGraph g;
+  // ListGraph::EdgeMap<int> map(g);
+  // TestGraphs test(&g, &map);
+  // test.createBicycleWheel(8);
+  // ListGraph* t;
+  // ListGraph a;
+  // ExtendedListGraphBase::NodeMap<ListGraphBase::Node> nr(g);
+  // ExtendedListGraphBase::EdgeMap<ListGraphBase::Edge> ecr(a);
+  // GraphCopy<ListGraph, ListGraph> gc(g, a);
+  // gc.nodeRef(nr).edgeCrossRef(ecr);
+  // gc.run();
+  // t = &a;
+  // int c = 0;
+  return 0;
+};
